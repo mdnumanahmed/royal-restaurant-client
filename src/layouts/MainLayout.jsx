d487cdfbd603de3eab14a730fefa81013ec5fac9
mdnumanahmed <mnumansyl@gmail.com>
2023-05-23 19:06:35 +0600

    project setup router, main layout and shared navbar footer added

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+
+import { Outlet } from 'react-router-dom';
+import NavBar from '../pages/Shared/NavBar/NavBar';
+import Footer from '../pages/Shared/Footer/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <NavBar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
+
+export default MainLayout;

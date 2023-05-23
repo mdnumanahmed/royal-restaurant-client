814901daafa249d2c55a92f6332e83537867de03
mdnumanahmed <mnumansyl@gmail.com>
2023-05-23 23:24:33 +0600

    fixed navbar and responsive react carousel added

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -33,7 +33,7 @@ const NavBar = () => {
   );
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-black opacity-70 fixed z-10 max-w-screen-xl text-white">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
